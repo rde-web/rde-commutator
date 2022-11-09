1013779f476c35f5d0978a075dd8455e10462e88
Калюжный Алексей <abkalyuzhn@mts.ru>
2022-11-09 18:37:33 +0300

    config

---
+++ include/rde-commutator/config.h
@@ -0,0 +1,10 @@
+// @todo from file
+#define DAEMON_SOCK_LOCATION "tmp/d"
+#define CLIENT_SOCK_LOCATION "tmp/cl"
+
+#define MAX_MSG_SIZE 256
+#define TCP_BACKLOG 128
+#define UNIX_BACKLOG 128
+
+#define REMOTE_REGISTER_OK 0
+#define REMOTE_REGISTER_FAIL 1
